c8a4b1c158daf1e119e5e04f73d1c08a5a694596
RavoniainaDarisse <ravoniainaheritiana@gmail.com>
2023-12-20 12:51:59 +0100

    Meteo 1

---
+++ src/environments/environment.d.ts
@@ -0,0 +1,8 @@
+export interface Environment {
+    weatherApiBaseUrl: string;
+    production: boolean;
+    XRapidAPIKeyHeaderName: string;
+    XRapidAPIKeyValue: string;
+    XRapidAPIHostHeaderName: string;
+    XRapidAPIHostValue: string;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment: any = {
+    production: false,
+    weatherApiBaseUrl: ' http://api.weatherapi.com/v1',
+
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'api.weatherapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyValue: 'ac4bbea9798d4fb19f770855232012'
+};
